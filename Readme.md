c6e9be2e2e5a1df216e42f2a9095e13c8b436cee
stu <sjmduncan@gmail.com>
2019-08-26 20:21:53 +1200

    Update readme. Add link to repository

--- Readme.md
+++ Readme.md
@@ -1,4 +1,23 @@
 # Javascript Noise Machine
 
-Generate and play volume-modulated coloured noise in your browser.
-Background playback in iOS
+[Demo Here](https://sjmduncan.github.io/noise/index.html).
+
+Once upon a time I used [ChromaDoze](https://github.com/pmarks-net/chromadoze) for all of my coloured noise needs.
+Then Android and I parted ways and haven't found a replacement noise machine. 
+
+This is my current replacement: a basic Javascript procedural noise machine with adjustable lowpass shaping (`whiteness`), sinusoidal volume modulation (`PULSE`), and most importantly background playback in iOS via (ab)use of the `<audio>` tag.
+
+
+## How
+
+1. Generate white noise buffer, apply sinusoidal amplituded modulation if `PULSE` is enabled. Re-generate the buffer when `PULSE` is enabled/disabled/modified
+2. Pass buffer to `AudioBufferSourceNode`
+3. Connect `AudioBufferSourceNode` to `BiquadFilterNode`
+4. Connect `BiquadFilterNode` to the `AudioDestinationNode` of the window AudioContext
+5. Sync the `AudioContext` play state (`suspend/resume`) with that of an `<audio>` element which just infinitely loops a short silent `.wav` file, otherwise mobile browsers will kill playback when you use other apps or lock the screen
+    1. Note that pause from the mobile UI will work but play/resume (probably) won't because Javascript execution is not allowed for background apps/browser tabs (at least under iOS), so play state sync will break once paused.
+
+The noise buffer is up to 30 seconds long, and the actual length varies depending on the maximum exact multiple (in sample counts) of the `PULSE` period which fits in the 30 second buffer (if `PULSE` is enabled, otherwise it's just 30 seconds of white noise).
+The initial `AudioBufferSource` has to be passed a full 30 second buffer because Safari (at least under iOS) doesn't like it when buffer sizes get bigger later (desktop firefox/chrome seem okay with that though).
+`PULSE` period starts at 0.2Hz to 5Hz (so you can do theta waves but not binaural ones, sorry), and is adjustable on a log scale.
+Whiteness ranges starts at 1 (lowpass cutoff=100Hz) and goes to 11 (lowpass cutoff=1.5kHz), because above that is too white for my personal taste.
--- noise.js
+++ noise.js
@@ -9,10 +9,16 @@ shaper.Q = 0.4
 audioContext.suspend()
 
 function generate_buffer(mod_period){
-    if(mod_period != 0)
-        var buffer_size = mod_period*2*audioContext.sampleRate
+    //TODO Generate shaped noise, avoid biquadfilter
+    var bloop = 30
+    if(mod_period != 0){
+        var periods = Math.floor(bloop/mod_period)
+        var buffer_size = periods*mod_period*audioContext.sampleRate
+        console.log(periods)
+        console.log(bloop/mod_period)
+    }
     else{
-        var buffer_size = 2*audioContext.sampleRate
+        var buffer_size = bloop*audioContext.sampleRate
     }
     var buffer = audioContext.createBuffer(1, buffer_size, audioContext.sampleRate)
     var buffer_data = buffer.getChannelData(0)
@@ -25,7 +31,7 @@ function generate_buffer(mod_period){
         var samples_period = mod_period*audioContext.sampleRate
         var sin_scale =  (2 * Math.PI) / samples_period
         for (var i = 0; i < buffer_size; i++) {
-            vol_scale = 0.5*Math.sin(sin_scale * i) + 1
+            vol_scale = 0.4*Math.sin(sin_scale * i) + 1.2
             buffer_data[i] = (Math.random() * 2 - 1) * vol_scale;
         }
     }
@@ -33,7 +39,7 @@ function generate_buffer(mod_period){
 }
 
 var white_noise = audioContext.createBufferSource();
-white_noise.buffer = generate_buffer(1/0.01);
+white_noise.buffer = generate_buffer(1.0/Math.exp(-1.6));
 white_noise.loop = true;
 white_noise.start(0);
 white_noise.connect(shaper);
@@ -41,58 +47,73 @@ white_noise.connect(shaper);
 white_noise.buffer = generate_buffer(0);
 
 function get_pulse_hz(){
-    period=$("#pulsePeriod").val()
-    return 1.0/period
+    selected=$("#pulsePeriod")[0].value
+    return Math.exp(selected)
 }
 
-function play(){
-    $("#audio")[0].play();
-    audioContext.resume()
+function get_whitenes_factor(){
+    //TODO: Adjust volume for whiter noises
+    return ($("#shaperFreq")[0].value-100)*(10/1400) + 1
 }
 
-function pause() {
-    $("#audio")[0].pause();
-    audioContext.suspend()
 
+function play_pause(){
+    if($("#butStart")[0].value == "START"){
+        $("#audio")[0].play();
+        audioContext.resume()
+        $("#butStart")[0].value = "STOP"
+        $("#butStart").removeClass("button-unselected")
+        $("#butStart").addClass("button-selected")
+    }else{
+        $("#audio")[0].pause();
+        audioContext.suspend()
+        $("#butStart")[0].value = "START"
+        $("#butStart").addClass("button-unselected")
+        $("#butStart").removeClass("button-selected")
+    }
+    
 }
-var is_pulsing = false
+
+var defaultButtonColor
 function pulse_enable(){
-    if(!is_pulsing){
+    if($("#pulseEnable")[0].value == "PULSE"){
         volmod_hz = get_pulse_hz()
-        white_noise.buffer = generate_buffer(volmod_hz);
-        $("#pulseEnable").val("!PULSE!")
+        white_noise.buffer = generate_buffer(1.0/volmod_hz);
+        $("#pulseEnable")[0].value = "UNPULSE"
+        $("#pulseEnable").removeClass("button-unselected")
+        $("#pulseEnable").addClass("button-selected")
     }else{
         white_noise.buffer = generate_buffer(0);
-        $("#pulseEnable").val("PULSE")
+        $("#pulseEnable")[0].value = "PULSE"
+        $("#pulseEnable").removeClass("button-selected")
+        $("#pulseEnable").addClass("button-unselected")
     }
-    is_pulsing = !is_pulsing;
 }
 
 function whiteness_drag(){
-    $("#whiteness")[0].innerText = "whiteness " + (1*$("#shaperFreq")[0].value).toFixed(0)
+    $("#whiteness")[0].innerText = "whiteness " + get_whitenes_factor().toFixed(2)
 }
 
 function pulse_drag(){
-    $("#pulseHz")[0].innerText = "pulse " + (1*$("#pulsePeriod")[0].value).toFixed(2) + "Hz"
+    $("#pulseHz")[0].innerText = "pulse " + get_pulse_hz().toFixed(2) + "Hz"
 }
 
 function filter_update(){
-    $("#whiteness")[0].innerText = "whiteness " + (1*$("#shaperFreq")[0].value).toFixed(0)
+    $("#whiteness")[0].innerText = "whiteness " + get_whitenes_factor().toFixed(2)
     shaper.frequency.value=$("#shaperFreq")[0].value
 }
 
 function update_pulse(){
     volmod_hz = get_pulse_hz()
-    $("#pulseHz")[0].innerText = "pulse " + (1*$("#pulsePeriod")[0].value).toFixed(2) + "Hz"
-    if(volmod_hz < 0.01){
-        $("#pulsePeriod").val(0.01)
-    }
-    else if(is_pulsing){
-        white_noise.buffer = generate_buffer(volmod_hz);
+    $("#pulseHz")[0].innerText = "pulse " + volmod_hz.toFixed(2) + "Hz"
+    if($("#pulseEnable")[0].value != "PULSE"){
+        white_noise.buffer = generate_buffer(1.0/volmod_hz);
     }
 }
 
 function init(){
-    $("#pulseHz")[0].innerText = "pulse " + (1*$("#pulsePeriod")[0].value).toFixed(2) + "Hz"
-    $("#whiteness")[0].innerText = "whiteness " + (1*$("#shaperFreq")[0].value).toFixed(0)
+    $("#pulseHz")[0].innerText = "pulse " + get_pulse_hz().toFixed(2) + "Hz"
+    $("#whiteness")[0].innerText = "whiteness " + get_whitenes_factor().toFixed(2)
+    shaper.frequency.value = $("#shaperFreq")[0].value
+    defaultButtonColor = $("#pulseEnable")[0].style.backgroundColor
 }
